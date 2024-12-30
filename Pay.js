e8e14e467ead8a96dbdbefb5654ff5b3d63016fb
Nirjhar Debnath <nirjhardebnath2006@gmail.com>
2024-12-30 23:10:18 +0530

    mod

---
+++ Pay.js
@@ -0,0 +1,12 @@
+// document.getElementById('upi-payment-form').addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     const upiId = document.getElementById('upi-id').value;
+//     const amount = document.getElementById('amount').value;
+
+//     // Construct UPI payment URI
+//     const upiUri = `upi://pay?pa=${upiId}&pn=ArtistName&mc=0000&tid=1234567890&tr=txn12345&tn=Payment%20for%20artwork&am=${amount}&cu=INR`;
+
+//     // Redirect to UPI app
+//     window.location.href = upiUri;
+// // Initialize EmailJS
